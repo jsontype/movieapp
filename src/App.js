f604b0d91f98792e1e682e235034f82d96465bb3
littledrum070 <littledrum070@gmail.com>
2022-07-21 22:16:35 +0900

    feat: 영화 장르의 표시

--- src/App.js
+++ src/App.js
@@ -16,7 +16,9 @@ function App() {
     return (
       <div className='movie' key={item.title}>
         <img className='movieImage' src={item.large_cover_image} alt={item.title}></img>
-        <a className='movieTitle' href={item.url}>{item.title}</a>
+        <a className='movieTitle' href={item.url}>{item.title}
+        <div className='movieGenres'>{[item.genres].join(', ')}</div>
+        </a>
         <div className='movieYear'>{item.year}</div>
       </div>
     )
